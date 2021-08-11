b6773d274bf185634a890228039b578ed7f838c8
salybu <rkdmsdl93@gmail.com>
2021-08-11 13:10:19 +0900

    [FEAT] Route 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Edit from "./pages/Edit";
+import Detail from "./pages/Detail";
+import Add from "./pages/Add";
+import Signin from "./pages/Signin";
+import NotFound from "./pages/NotFound";
+import Error from "./pages/Error";
+import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ErrorBoundary FallbackComponent={Error}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/edit/:id" component={Edit} />
+          <Route exact path="/book/:id" component={Detail} />
+          <Route exact path="/add" component={Add} />
+          <Route exact path="/signin" component={Signin} />
+          <Route exact path="/" component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 
---
+++ src/redux/modules/auth.ts
@@ -0,0 +1,24 @@
+import { createActions, handleActions } from "redux-actions";
+
+interface AuthState {
+  token: string | null;
+  loading: boolean;
+  error: Error | null;
+}
+
+const initialState: AuthState = {
+  token: null,
+  loading: false,
+  error: null,
+};
+
+const prefix = "my-books/auth";
+
+export const { pending, success, fail } = createActions(
+  "PENDING",
+  "SUCCESS",
+  "FAIL",
+  { prefix }
+);
+
+const reducer = handleActions;
