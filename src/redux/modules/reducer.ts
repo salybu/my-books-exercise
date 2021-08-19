b0e94196e7fa89e1287a5864269f0dc7d0f9dd7f
salybu <rkdmsdl93@gmail.com>
2021-08-19 15:43:28 +0900

    [FEAT] router with redux 관련 코드 추가

--- src/redux/modules/reducer.ts
+++ src/redux/modules/reducer.ts
@@ -1,8 +1,14 @@
+import { connectRouter } from "connected-react-router";
 import { combineReducers } from "redux";
 import auth from "./auth";
+import books from "./books";
+import { History } from "history";
 
-const reducer = combineReducers({
-  auth,
-});
+const reducer = (history: History<unknown>) =>
+  combineReducers({
+    auth,
+    books,
+    router: connectRouter(history),
+  });
 
 export default reducer;
